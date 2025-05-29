06cd6c6c4f0d0bd13dfdc0890f3b505da0f1caf9
Jingyan Zhang <jingyan.zhang@ain.polytechnique.fr>
2025-05-29 19:04:46 +0200

    add data augmentation

--- data.py
+++ data.py
@@ -9,3 +9,14 @@ dataset = tf.keras.utils.image_dataset_from_directory(
     batch_size=32,                  
     shuffle=True                    
 )
+
+from tensorflow.keras import layers
+
+data_augmentation = tf.keras.Sequential([
+    layers.RandomFlip("horizontal"),
+    layers.RandomRotation(0.1),
+    layers.RandomZoom(0.1),
+    layers.RandomContrast(0.1),
+])
+
+augmented_dataset = dataset.map(lambda x, y: (data_augmentation(x, training=True), y))
