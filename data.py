0b7bc06c29f36c889078beaf8cc9a71217b619a2
Jingyan Zhang <jingyan.zhang@ain.polytechnique.fr>
2025-05-31 14:58:22 +0200

    modifty data and model

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+from logging.config import valid_ident
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
@@ -7,8 +8,30 @@ dataset = tf.keras.utils.image_dataset_from_directory(
     directory='data/',
     labels='inferred',              
     label_mode='int',               
-    batch_size=32,                  
+    batch_size=32,           
     shuffle=True                    
+)
+
+train_dataset = tf.keras.utils.image_dataset_from_directory(
+    directory='data/',
+    validation_split = 0.2,
+    subset = "training",
+    seed = 123,
+    labels='inferred',              
+    label_mode='int',               
+    batch_size=32,           
+    shuffle=True                 
+)
+
+val_dataset = tf.keras.utils.image_dataset_from_directory(
+    directory='data/',
+    validation_split = 0.2,
+    subset = "validation",
+    seed = 123,
+    labels='inferred',              
+    label_mode='int',               
+    batch_size=32,           
+    shuffle=True                 
 )
 
 data_augmentation = tf.keras.Sequential([
@@ -16,6 +39,9 @@ data_augmentation = tf.keras.Sequential([
     layers.RandomRotation(0.1),
     layers.RandomZoom(0.1),
     layers.RandomContrast(0.1),
+    layers.Rescaling(1./255),
 ])
 
 augmented_dataset = dataset.map(lambda x, y: (data_augmentation(x, training=True), y))
+augmented_training_dataset = train_dataset.map(lambda x, y: (data_augmentation(x, training=True), y))
+augmented_val_dataset = val_dataset.map(lambda x, y: (data_augmentation(x, training=True), y))
--- model.py
+++ model.py
@@ -6,11 +6,15 @@ def block(x, filters, name=None):
     shortcut = x
     x = layers.Conv2D(filters, kernel_size=3, padding='same', activation='relu')(x)
     x = layers.BatchNormalization()(x)
-    x = layers.Dropout(0.1)(x)
+    x = layers.Dropout(0.1)(x)   #randomly forget 10% of the output of the previous layer
     x = layers.Conv2D(filters, kernel_size=3, padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.Dropout(0.1)(x)
-    x = layers.add([x, shortcut])
+
+    if shortcut.shape[-1] != filters:
+        shortcut = layers.Conv2D(filters, kernel_size=1, padding='same')(shortcut)
+    x = layers.add([x, shortcut])    # stablize the gradient 
+    x = layers.Activation("relu")(x)
     x = layers.Conv2D(2 * filters, kernel_size=3, padding='same', activation='relu')(x)
     x = layers.BatchNormalization()(x)
     x = layers.Dropout(0.1)(x)
@@ -18,11 +22,13 @@ def block(x, filters, name=None):
     return x
 
 def build_model():
-    model_input = layers.Input((120, 150))
+    model_input = layers.Input((256, 256, 3))
+    #x = layers.Reshape((255,255,3))(model_input)
     x = block(model_input, 64)
     x = block(x, 128)
     x = block(x, 256)
-    x = block(x, 256)
+    x = block(x, 512)  # (batch_size, A, B, C)
+    x = layers.GlobalAveragePooling2D()(x)
     x = layers.Dense(1024, activation="relu")(x)
     x = layers.BatchNormalization()(x)
     x = layers.Dropout(0.1)(x)
@@ -30,9 +36,9 @@ def build_model():
     x = layers.BatchNormalization()(x)
     x = layers.Dropout(0.1)(x)
     x = layers.Dense(256, activation="relu")(x)
-    x = layers.BatchNormalization()
-    x = layers.Dense(100, activation="sigmoid")
-    x = layers.Softmax()(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Dense(100, activation="softmax")(x)
+    #x = layers.Softmax()(x)
     model = keras.Model(model_input, x)
-    model.compile(loss="sparse_categorical_crossentropy", metrics=["loss"])
+    model.compile(loss="sparse_categorical_crossentropy", metrics=["accuracy"])
     return model
